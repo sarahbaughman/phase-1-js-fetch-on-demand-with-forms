b3c1196cca2054eed8bf8018f7d19bb2fa65dd0a
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-26 13:19:42 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 const init = () => {
+  const inputForm = document.querySelector("form");
   
+  inputForm.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const input = document.querySelector("input#searchByID")
+
+//Fetch data based on user input 
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then ((response) => response.json())
+        .then ((data) => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+            
+            title.innerText = data.title
+            summary.innerText= data.summary
+            
+            console.log(data)
+         })
+
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+
+//OR event.target.children[1].value
+//Handles the event, ie takes the text, makes it a list item,posts it to the site
